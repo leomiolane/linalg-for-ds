7b09f539852420b024259f903042c9d557ce730d
homerOurs <homerours@gmail.com>
2019-05-14 22:12:38 -0700

    outline generator

--- generate_outline.py
+++ generate_outline.py
@@ -1,11 +1,26 @@
 import os
 import re
 
+def correct_ref(section):
+    if '\\ref{' in section:
+        return section + '}'
+    else:
+        return section
+
+def title_item_tex(title):
+    return item_tex('\\textsc{' + title + '}')
+
+def item_tex(title):
+    return '\\item ' + title + '\n'
+
 
 N = 0
-look_for_next_lecture = True
+folder_exists = True
 
-while (look_for_next_lecture):
+outline_tex = open('outline_content.tex', 'w')
+outline_tex.write('\\begin{enumerate}[label=\\textbf{\\arabic*.}]\n')
+
+while (folder_exists):
     N += 1
     if N < 10:
         nb = '0' + str(N)
@@ -15,13 +30,26 @@ while (look_for_next_lecture):
     folder_name = 'lecture_' + nb
     # Check if lecture folder exists
     folder_exists = os.path.isdir(folder_name)
-    print folder_exists
-    look_for_next_lecture = folder_exists
+    if folder_exists:
+        # Read Titlek
+        tex_file = open(folder_name + '/' + folder_name + '.tex').read()
+        # print tex_file.readline()
+        search = 'Lecture ' + str(N) + ': ' + '(.*?)\}'
+        title = re.findall(search, tex_file, re.S)
 
-    # Read Titlek
-    tex_file = open(folder_name + '/' + folder_name + '.tex').read()
-    # print tex_file.readline()
-    search = 'Lecture ' + str(N) + ': ' + '(.*?)\}'
-    title = re.findall(search, tex_file, re.S)
-    print title
-print N
+        search = 'section{(.*?)}'
+        sections = re.findall(search, tex_file, re.S)
+
+        outline_tex.write(title_item_tex(title[0]))
+        outline_tex.write('\\begin{enumerate}[label=\\arabic*.]\n')
+
+        for i in range(len(sections)):
+            sections[i] = correct_ref(sections[i])
+            outline_tex.write(item_tex(sections[i]))
+        outline_tex.write('\\end{enumerate}\n')
+         
+        # print sections
+        # print title
+
+outline_tex.write('\\end{enumerate}\n')
+print 'Done!'
